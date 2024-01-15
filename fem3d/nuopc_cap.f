d2d14e0ee69f82e93a2bd77c4c2f913556a40b11
Georg Umgiesser <georg.umgiesser@ismar.cnr.it>
2024-01-15 18:27:17 +0100

    Momemtum stress and atmospheric pressure imported

--- fem3d/nuopc_cap.f
+++ fem3d/nuopc_cap.f
@@ -127,12 +127,7 @@
 	  !! to call also the SHYFEM initialization. Here the grid and data are read,
 	  !! the initial conditions are set and data structures are initialized:  
 	  call shyfem_initialize
-
 	  call ESMF_LogWrite("Initialized OCN", ESMF_LOGMSG_INFO, rc=rc)
-	  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
-     +	    line=__LINE__,
-     +	    file=__FILE__))
-     +	    return  ! bail out
       
 	  ! query for importState and exportState
 	  call NUOPC_ModelGet(model, importState=importState,
@@ -142,12 +137,16 @@
      +	    file=__FILE__))
      +	    return  ! bail out
 
-	  ! Disabling the following macro, e.g. renaming to WITHIMPORTFIELDS_disable,
-	  ! will result in a model component that does not advertise any importable
-	  ! Fields. Use this if you want to drive the model independently.
+	  !! We have added a macro to rapidly decouple the ocean component from the rest
+          !! of the earth system.  Disabling the following macro, e.g. renaming to WITHIMPORTFIELDS_disable,
+	  !! will result in an ocean component that does not advertise any importable
+	  !! Fields. Use should you this only if you want to drive the model independently.
 #define WITHIMPORTFIELDS
 #ifdef WITHIMPORTFIELDS
-	  ! importable field: air_pressure_at_sea_level
+	  !! The following are the standard variable that are imported in the 
+	  !! ocean component. 
+	  !! We have the atmospheric pressure at sea-level $p_a$ that
+	  !! act as a forcing term on the momentum equation.
 	  call NUOPC_Advertise(importState,
      +	    StandardName="air_pressure_at_sea_level", name="pmsl", rc=rc)
 	  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
@@ -155,7 +154,7 @@
      +	    file=__FILE__))
      +	    return  ! bail out
 
-	  ! importable field: surface_net_downward_shortwave_flux
+          !! The next is the net radiation flux $R$.
 	  call NUOPC_Advertise(importState,
      +	    StandardName="surface_net_downward_shortwave_flux", 
      +      name="rsns", rc=rc)
@@ -163,9 +162,31 @@
      +	    line=__LINE__,
      +	    file=__FILE__))
      +	    return  ! bail out
+
+	  !! We have the ocean-atmosphere flux $F_{oa}(U^{n}_o,U^{n}_a)$ 
+          !! that describes the flux across the surface for momentum (wind stress) 
+	  !! and temperature (heat flux). These fluxes are typically computed by the
+	  !! the atmospheric component with the aid of bulk formulae.
+          !! For now we have added only momentum flux that has two components.
+          call NUOPC_Advertise(importState,
+     +      StandardName="surface_downward_eastward_stress",
+     +      name="smes", rc=rc)
+          if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +      line=__LINE__,
+     +      file=__FILE__))
+     +      return  ! bail out
+
+          call NUOPC_Advertise(importState,
+     +      StandardName="surface_downward_northward_stress",
+     +      name="smns", rc=rc)
+          if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +      line=__LINE__,
+     +      file=__FILE__))
+     +      return  ! bail out
 #endif
 
-	  ! exportable field: sea_surface_temperature
+	  !! As output the ocean state at the surface layer must be 
+	  !! exorted. For now we have exported only the sea surface temperature.
 	  call NUOPC_Advertise(exportState,
      +	    StandardName="sea_surface_temperature", name="sst", rc=rc)
 	  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
@@ -211,9 +232,9 @@
 	  !! a lat-lon grid), the typical choice is \textsf{ESMF\_Grid}. For unstructured
 	  !! grids, use an \textsf{ESMF\_Mesh}. 
 	  ! create a Grid object for Fields
-	  gridIn = ESMF_GridCreateNoPeriDimUfrm(maxIndex=(/100, 10/),
-     +	    minCornerCoord=(/10._ESMF_KIND_R8, 20._ESMF_KIND_R8/),
-     +	    maxCornerCoord=(/100._ESMF_KIND_R8, 200._ESMF_KIND_R8/),
+	  gridIn = ESMF_GridCreateNoPeriDimUfrm(maxIndex=(/10, 10/),
+     +	minCornerCoord=(/0._ESMF_KIND_R8, 0._ESMF_KIND_R8/),
+     +	maxCornerCoord=(/100000._ESMF_KIND_R8, 100000._ESMF_KIND_R8/),
      +	    coordSys=ESMF_COORDSYS_CART, 
      +      staggerLocList=(/ESMF_STAGGERLOC_CENTER/),
      +	    rc=rc)
@@ -223,40 +244,94 @@
      +	    return  ! bail out
 	  gridOut = gridIn ! for now out same as in
 
+
+          !! An ESMF Field represents a physical field, such as temperature. 
+          !! The motivation for including Fields in ESMF is that bundles of Fields 
+          !! are the entities that are normally exchanged when coupling Components.
+          !! The ESMF Field class contains distributed and discretized field data, 
+          !! a reference to its associated grid, and metadata. The Field class stores 
+          !! the grid staggering for that physical field. This is the relationship 
+          !! of how the data array of a field maps onto a grid (e.g. one item per cell 
+          !! located at the cell center, one item per cell located at the NW corner, 
+          !! one item per cell vertex, etc.). This means that different Fields which 
+          !! are on the same underlying ESMF Grid but have different staggerings can 
+          !! share the same Grid object without needing to replicate it multiple times.
+          !! Fields can be added to States for use in inter-Component data 
+          !! communications. Fields can also be added to FieldBundles, which are 
+          !! groups of Fields on the same underlying Grid. One motivation for packing 
+          !! Fields into FieldBundles is convenience; another is the ability to perform 
+          !! optimized collective data transfers.
+          !! Field communication capabilities include: data redistribution, regridding, 
+          !! scatter, gather, sparse-matrix multiplication, and halo update. These are 
+          !! discussed in more detail in the documentation for the specific method calls. 
+          !! ESMF does not currently support vector fields, so the components of a 
+          !! vector field must be stored as separate Field objects.
+
+	  !! With the following commands we create a field for the pressure. Fields
+	  !! in ESMF are of type \textsf{ESMF\_field}.
+	  !! The subroutine \textsf{ESMF\_FieldCreate} simply associates the data 
+	  !! with the Grid. The keywords are very important. The keyword \textsf{staggerloc}
+	  !! specifies how the data is attached to the grid. \textsf{typekind} tells about 
+          !! the data type, in this case double precision. The name that have been advertised must be 
+	  !! also appended. 
 #ifdef WITHIMPORTFIELDS
-	  ! importable field: air_pressure_at_sea_level
+
+          !! An \textsf{ESMF\_Field} is created by passing the field name 
+          !! (should be the same as advertised), the grid, and the data type of the 
+          !! field to \textsf{ESMF\_FieldCreate}. 
 	  field = ESMF_FieldCreate(name="pmsl", grid=gridIn,
-     +	    typekind=ESMF_TYPEKIND_R8, rc=rc)
+     +	    typekind=ESMF_TYPEKIND_R8, staggerloc=ESMF_STAGGERLOC_CENTER, 
+     +      rc=rc)
 	  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
      +	    line=__LINE__,
      +	    file=__FILE__))
      +	    return  ! bail out
+          !! Fields are put into import or export States by calling 
+          !! \textsf{NUOPC\_Realize}.
 	  call NUOPC_Realize(importState, field=field, rc=rc)
 	  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
      +	    line=__LINE__,
      +	    file=__FILE__))
      +	    return  ! bail out
 
-	  !! An \textsf{ESMF\_Field} is created by passing the field name 
-	  !! (should be the same as advertised), the grid, and the data type of the 
-	  !! field to \textsf{ESMF\_FieldCreate}. 
-	  ! importable field: surface_net_downward_shortwave_flux
+	  !! The others fields follow identically:
 	  field = ESMF_FieldCreate(name="rsns", grid=gridIn,
      +	    typekind=ESMF_TYPEKIND_R8, rc=rc)
 	  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
      +	    line=__LINE__,
      +	    file=__FILE__))
      +	    return  ! bail out
-	  !! Fields are put into import or export States by calling 
-	  !! \textsf{NUOPC\_Realize}.
 	  call NUOPC_Realize(importState, field=field, rc=rc)
 	  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
      +	    line=__LINE__,
      +	    file=__FILE__))
      +	  return  ! bail out
+
+          field = ESMF_FieldCreate(name="smes", grid=gridIn,
+     +      typekind=ESMF_TYPEKIND_R8, rc=rc)
+          if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +      line=__LINE__,
+     +      file=__FILE__))
+     +      return  ! bail out
+          call NUOPC_Realize(importState, field=field, rc=rc)
+          if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +      line=__LINE__,
+     +      file=__FILE__))
+     +    return  ! bail out
+
+          field = ESMF_FieldCreate(name="smns", grid=gridIn,
+     +      typekind=ESMF_TYPEKIND_R8, rc=rc)
+          if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +      line=__LINE__,
+     +      file=__FILE__))
+     +      return  ! bail out
+          call NUOPC_Realize(importState, field=field, rc=rc)
+          if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +      line=__LINE__,
+     +      file=__FILE__))
+     +    return  ! bail out
 #endif
 
-	  ! exportable field: sea_surface_temperature
 	  field = ESMF_FieldCreate(name="sst", grid=gridOut,
      +	    typekind=ESMF_TYPEKIND_R8, rc=rc)
 	  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
@@ -307,6 +382,17 @@
 	  character(len=160)          :: msgString
           double precision            :: timeStepSec
 
+!! Work in progress !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+	  type(ESMF_Field)            :: field
+	  double precision, pointer   :: pmslPtr(:,:)
+          double precision, pointer   :: smesPtr(:,:)
+          double precision, pointer   :: smnsPtr(:,:)
+	  integer                     :: totalLBnd(2)
+	  integer                     :: totalUBnd(2)
+	  integer                     :: total_count(2)
+	  integer                     :: i,j
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 #define NUOPC_TRACE__OFF
 #ifdef NUOPC_TRACE
 	  call ESMF_TraceRegionEnter("OCN:Advance")
@@ -321,6 +407,61 @@
      +	    line=__LINE__,
      +	    file=__FILE__))
      +	    return  ! bail out
+
+!! WORK IN PROGRESS
+
+	  call ESMF_StatePrint(importState, rc=rc)
+
+	  call ESMF_StateGet(importState, field=field, 
+     +      itemName="pmsl", rc=rc)
+	  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +	    line=__LINE__,
+     +	    file=__FILE__))
+     +	    return  ! bail out
+          
+	  call ESMF_FieldGet(field, localDe=0, farrayPtr=pmslPtr,
+     +	    totalLBound=totalLBnd, totalUBound=totalUBnd,
+     +	    totalCount=total_count, rc=rc)
+	  if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +	    line=__LINE__,
+     +	    file=__FILE__))
+     +	    return  ! bail out
+
+          call ESMF_StateGet(importState, field=field, 
+     +      itemName="smes", rc=rc)
+          if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +      line=__LINE__,
+     +      file=__FILE__))
+     +      return  ! bail out
+          
+          call ESMF_FieldGet(field, localDe=0, farrayPtr=smesPtr,
+     +      totalLBound=totalLBnd, totalUBound=totalUBnd,
+     +      totalCount=total_count, rc=rc)
+          if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +      line=__LINE__,
+     +      file=__FILE__))
+     +      return  ! bail out
+
+          call ESMF_StateGet(importState, field=field,
+     +      itemName="smns", rc=rc)
+          if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +      line=__LINE__,
+     +      file=__FILE__))
+     +      return  ! bail out
+
+          call ESMF_FieldGet(field, localDe=0, farrayPtr=smnsPtr,
+     +      totalLBound=totalLBnd, totalUBound=totalUBnd,
+     +      totalCount=total_count, rc=rc)
+          if (ESMF_LogFoundError(rcToCheck=rc, msg=ESMF_LOGERR_PASSTHRU,
+     +      line=__LINE__,
+     +      file=__FILE__))
+     +      return  ! bail out
+
+	  print *, "OCN: pmsl -> ", pmslPtr
+          print *, "OCN: smes -> ", smesPtr
+          print *, "OCN: smns -> ", smnsPtr
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 	  !! We access to the clock object in different ways: here we
 	  !! output the current time to the log file:
